237c8a808c3c4578518f7ab32214359bf963a4a8
dogzeira <102675022+dogzeira@users.noreply.github.com>
2022-05-04 23:24:37 -0300

    Add files via upload

---
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.generation.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto , Long> {
+	public List<Produto> findAllByConsoleContainingIgnoreCase(@Param("console") String console);
+
+	public List<Produto> findByPrecoGreaterThan(Double preco);
+
+    public List<Produto> findAllByPrecoLessThan(Double preco);
+}
